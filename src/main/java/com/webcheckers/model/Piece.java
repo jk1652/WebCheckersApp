387a46d5259ba66648b9573fff7ee26130a303bf
Quentin Ramos II <qkr2678@rit.edu>
2021-10-01 21:50:20 -0400

    Initial Piece Class

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -6,4 +6,30 @@ public class Piece {
     // Attributes
     //
 
+    private enum Type {SINGLE, KING}
+    private final Type type;
+
+    private enum Color {RED, WHITE}
+    private final Color color;
+    //
+    // Constructor
+    //
+
+    public Piece(Type type, Color color){
+        this.type = type;
+        this.color = color;
+    }
+
+    //
+    // Public Methods
+    //
+
+    public Type getType(){
+        return type;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
 }
