33057dacae634a696e4b6079fc8d9ca8fdef192f
zkk4285 <zkk4285@rit.edu>
2021-10-01 22:59:09 -0400

    create player lobby

---
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -0,0 +1,18 @@
+package com.webcheckers.appl;
+
+import com.webcheckers.model.Player;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class PlayerLobby {
+    private static final Logger LOG = Logger.getLogger(PlayerLobby.class.getName());
+
+    private List<Player> names;
+
+
+
+
+
+
+}
