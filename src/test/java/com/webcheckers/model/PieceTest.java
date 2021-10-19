c91ae0c06ef2df80533033a48b540898986b6e66
Quentin Ramos II <qkr2678@rit.edu>
2021-10-18 21:00:37 -0400

    finished the tests for webserver and piece

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -1,16 +1,54 @@
 package com.webcheckers.model;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Tag("Model-tier")
 class PieceTest {
 
     @Test
-    void getType() {
+    public void test_redPiece() {
+        final Piece CuT = new Piece(Piece.Type.SINGLE, Piece.Color.RED);
+
+        // check if color is red
+        assertEquals(Piece.Color.RED, CuT.getColor());
+
+        // check if piece is single
+        assertEquals(Piece.Type.SINGLE,CuT.getType());
     }
 
     @Test
-    void getColor() {
+    public void test_whitePiece() {
+        final Piece CuT = new Piece(Piece.Type.SINGLE, Piece.Color.WHITE);
+
+        // check if color is red
+        assertEquals(Piece.Color.WHITE, CuT.getColor());
+
+        // check if piece is single
+        assertEquals(Piece.Type.SINGLE,CuT.getType());
+    }
+
+    @Test
+    public void test_redKing() {
+        final Piece CuT = new Piece(Piece.Type.KING, Piece.Color.RED);
+
+        // check if color is red
+        assertEquals(Piece.Color.RED, CuT.getColor());
+
+        // check if piece is king
+        assertEquals(Piece.Type.KING,CuT.getType());
+    }
+
+    @Test
+    public void test_whiteKing() {
+        final Piece CuT = new Piece(Piece.Type.KING, Piece.Color.WHITE);
+
+        // check if color is red
+        assertEquals(Piece.Color.WHITE, CuT.getColor());
+
+        // check if piece is king
+        assertEquals(Piece.Type.KING,CuT.getType());
     }
 }
