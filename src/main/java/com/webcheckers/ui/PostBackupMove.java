6094794bda68310a79c80c8aeac744141393496f
Jaden KL <jk1652@rit.edu>
2021-10-24 03:43:46 -0400

    started PostBackupMove and PostSubmitTurn

---
+++ src/main/java/com/webcheckers/ui/PostBackupMove.java
@@ -0,0 +1,38 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.appl.GameManager;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Game;
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static spark.Spark.halt;
+
+public class PostBackupMove implements Route {
+
+    private final GameManager gameManager;
+    private final TemplateEngine templateEngine;
+
+    PostBackupMove(final GameManager gameManager, final TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(gameManager, "game must not be null");
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+
+        this.gameManager = gameManager;
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+
+        String playerName = request.session().attribute(PostSignInRoute.USERNAME);
+
+        Game game = gameManager.findPlayerGame(playerName);
+
+        return null;
+
+    }
+}
