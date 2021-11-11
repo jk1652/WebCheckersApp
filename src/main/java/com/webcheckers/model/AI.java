b02466f526285744417c92161cfc9d3323453b31
Quentin Ramos II <qkr2678@rit.edu>
2021-11-10 19:50:44 -0500

    Logic is now simpler and easier to comprehend

--- src/main/java/com/webcheckers/model/AI.java
+++ src/main/java/com/webcheckers/model/AI.java
@@ -19,21 +19,16 @@ public class AI extends Player{
 
         //get a list of all valid simple moves
         ArrayList<Move> moves = new ArrayList<>();
+        ArrayList<Move> jumps = new ArrayList<>();
         for (int a = 0; a < 8; a++) {for (int b = 0; b < 8; b++) {
             Piece piece = board.getRow(a).getSpace(b).getPiece();
             Position start = new Position(a,b);
             if(piece.getColor() == Piece.Color.WHITE){
+                //If there is a jump add the jump
+                if(game.canJump(start)){jumps.addAll(game.getPossibleJumps());}
+                //else if add valid moves a piece can make
                 ArrayList<Move> tempMoves = game.canMove(start);
                 moves.addAll(tempMoves);
-        }}}
-
-        //get a list of all valid jumps if there are any
-        ArrayList<Move> jumps = new ArrayList<>();
-        for (int x = 0; x < 8; x++) {for (int y = 0; y < 8; y++) {
-            Piece piece = board.getRow(x).getSpace(y).getPiece();
-            Position current = new Position(x,y);
-            if(piece.getColor() == Piece.Color.WHITE && game.canJump(current)){
-                jumps.addAll(game.getPossibleJumps());
         }}}
 
         if(game.forceJump()){return jumps;}
