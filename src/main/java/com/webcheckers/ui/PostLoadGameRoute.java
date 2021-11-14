d3ca97163a9f1962193fc038aae6df7d1c7999ca
zanek <zkk4285@g.rit.edu>
2021-11-14 04:04:37 -0500

    added documentation to PostLoadGameRoute

--- src/main/java/com/webcheckers/ui/PostLoadGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostLoadGameRoute.java
@@ -16,15 +16,13 @@ import java.util.logging.Logger;
 import static spark.Spark.halt;
 
 public class PostLoadGameRoute implements Route {
-    public static final String OPPONENT_NAME = "opponentName";
-    public static final String GAME_ID_ATTRIBUTE = "gameID";
     private final PlayerLobby playerLobby;
     private final GameManager gameManager;
     private final TemplateEngine templateEngine;
     private static final Logger LOG = Logger.getLogger(PlayerLobby.class.getName());
 
     /**
-     * The constructor for the {@code POST /game} route handler.
+     * The constructor for the {@code POST /load} route handler.
      * @param templateEngine
      * @param playerLobby
      * @param gameManager
@@ -35,40 +33,31 @@ public class PostLoadGameRoute implements Route {
         Objects.requireNonNull(playerLobby, "playerLobby must not be null");
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         Objects.requireNonNull(gameManager, "gameManager must not be null");
-        //
+
         this.playerLobby = playerLobby;
         this.templateEngine = templateEngine;
         this.gameManager = gameManager;
     }
 
     /**
-     * sends game related information
+     * Load a game, based on button pressed on homepage
      * @param request
      * @param response
-     * @return game info
+     * @return nothing
      * @throws Exception
      */
     @Override
     public Object handle(Request request, Response response) throws Exception {
 
-
         String playerName = request.session().attribute(PostSignInRoute.USERNAME);
-        //String opponentName = request.queryParams(OPPONENT_NAME);
 
-        Set<String> attr = request.attributes();
-        for (String x: attr){
-            LOG.fine("attr: " + x);
-        }
-
-        String loadGame = request.queryParams("1");
-        LOG.fine("load game: " + loadGame);
-        Integer gameID = request.session().attribute(GAME_ID_ATTRIBUTE);
+        String loadGame = "";
 
         Player user = playerLobby.getPlayer(playerName);
 
-
         Set<String> keys = user.getSaved().keySet();
 
+        // find which button was pressed, so it loads the right game
         int i = 1;
         for (String x: keys){
             String test = request.queryParams(Integer.toString(i));
@@ -78,6 +67,7 @@ public class PostLoadGameRoute implements Route {
                 break;
             }
         }
+        LOG.fine("load game: " + loadGame);
 
         Game game = gameManager.LoadGame(user.getSaved().get(loadGame), user);
 
@@ -87,8 +77,9 @@ public class PostLoadGameRoute implements Route {
 
             Map<String, Object> vm = new HashMap<>();
 
+            // check if you are playing a game vs a real player
             if (!game.getSinglePlayer()) {
-
+                // check if that player is online
                 if (playerLobby.getPlayer(game.getOpponentName(playerName)) == null) {
                     gameManager.finishGame(playerName);
                     request.session().attribute("message", Message.error("Selected Player is not Online"));
@@ -97,6 +88,7 @@ public class PostLoadGameRoute implements Route {
                 }
             }
 
+            // unload saved game from the player's saved games
             user.removeSaveGame(loadGame);
 
             if (oppo != null){
